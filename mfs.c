3a9c74314363b397188697cafd08cdf9560fa16b
jhs2800 <jhs2800@users.noreply.github.com>
2020-05-01 23:14:55 -0500

    Update mfs.c

--- mfs.c
+++ mfs.c
@@ -62,19 +62,68 @@ uint8_t BPB_NumFATs;
 uint16_t BPB_RootEntCnt;
 uint32_t BPB_FATSz32;
 
-// **** Change names!!!!! 
+// ************************************
+// **** Change names on all var !!! ****** 
+// ************************************
 struct __attribute__((__packed__)) DirectoryEntry
 {
   char DIR_Name[11];
   uint9_t Dir_Attr;
   uint8_t Unused1[8];
-  
-}
+  uint16_t DIR_FirstClusterHigh;
+  uint8_t Unused[4];
+  uint16_t DIR_FirstClusterLow;
+  uint32_t DIR_FileSize;
+};
 
-struct DirectoryEntry directory[16];
+struct DirectoryEntry dir[16];
 
 int main()
 {
+
+  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+
+  while( 1 )
+  {
+    // Print out the mfs prompt
+    printf ("mfs> ");
+
+    // Read the command from the commandline.  The
+    // maximum command that will be read is MAX_COMMAND_SIZE
+    // This while command will wait here until the user
+    // inputs something since fgets returns NULL when there
+    // is no input
+    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+
+    /* Parse input */
+    char *token[MAX_NUM_ARGUMENTS];
+
+    int   token_count = 0;                                 
+                                                           
+    // Pointer to point to the token
+    // parsed by strsep
+    char *arg_ptr;                                         
+                                                           
+    char *working_str  = strdup( cmd_str );                
+
+    // we are going to move the working_str pointer so
+    // keep track of its original value so we can deallocate
+    // the correct amount at the end
+    char *working_root = working_str;
+
+    // Tokenize the input stringswith whitespace used as the delimiter
+    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    
   FILE *fp;
   fp - fopen ("fat32.img", "r");
   if(!fp)
@@ -124,56 +173,18 @@ int main()
 
   int i;
   for(i=0; i< 16; i++)
-  {
-    if
-    printf("File: %s\n", directory[i].DIR_Name);
+  { // Only print files or subdirectories. 
+    if( dir[i].Dir_Attr = 0x01 || dir[i].Dir_Attr = 0x10 || dir[i].Dir_Attr = 0x20 )
+    {
+      printf("File: %s\n", dir[i].DIR_Name);
+    }
   }
 
   fclose(fp);
   return 0;
 
 
-  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
-
-  while( 1 )
-  {
-    // Print out the mfs prompt
-    printf ("mfs> ");
-
-    // Read the command from the commandline.  The
-    // maximum command that will be read is MAX_COMMAND_SIZE
-    // This while command will wait here until the user
-    // inputs something since fgets returns NULL when there
-    // is no input
-    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
-
-    /* Parse input */
-    char *token[MAX_NUM_ARGUMENTS];
-
-    int   token_count = 0;                                 
-                                                           
-    // Pointer to point to the token
-    // parsed by strsep
-    char *arg_ptr;                                         
-                                                           
-    char *working_str  = strdup( cmd_str );                
-
-    // we are going to move the working_str pointer so
-    // keep track of its original value so we can deallocate
-    // the correct amount at the end
-    char *working_root = working_str;
-
-    // Tokenize the input stringswith whitespace used as the delimiter
-    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
-              (token_count<MAX_NUM_ARGUMENTS))
-    {
-      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
-      {
-        token[token_count] = NULL;
-      }
-        token_count++;
-    }
+  
 
     // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your shell functionality
